523122338d9069fdea97f9c15367a53a649686b5
Manjunath Irukulla <imanjunad@gmail.com>
2025-06-18 14:30:59 +0530

    HeroSection Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
-
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
   return <main>
     <NavBar/>
     <HeroSection/>
+    <div className='h-dvh border border-red-500' />
   </main>
 }
 
--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,26 +1,90 @@
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { useGSAP } from "@gsap/react";
 
 const HeroSection = () => {
-  return <section className="bg-main-bg">
-    <div className="hero-container">
-        <img src="/images/static-img.png" alt="hero-img" className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"/>
-    <div className="hero-content">
-        <div className="overflow-hidden">
-            <h1 className="hero-title">Freaking Delicious</h1>
-        </div>
-        <div style={{clipPath:"polygon(50% 0, 50% 0, 50% 100%, 50% 100%)"}} className="hero-text-scroll">
-            <div className="hero-subtitle"> 
-                <h1>Protein + Caffine</h1>
-            </div>
-        </div>
-        <h2>
-            Live life to the fullest with SPYLT: Shatter boredom and embrace your inner kid withh every deliciously smooth chug.
-        </h2>
-        <div className="hero-button">
-            <p>Chug a SPYLT</p>
-        </div>
-    </div>
-    </div>
-  </section>
-}
+  useGSAP(() => {
+    const titleSplit = SplitText.create(".hero-title", {
+      type: "chars",
+    });
 
-export default HeroSection
+    const tl = gsap.timeline({
+      delay: 1,
+    });
+
+    tl.to(".hero-content", {
+      opacity: 1,
+      y: 0,
+      ease: "power1.inOut",
+    })
+      .to(
+        ".hero-text-scroll",
+        {
+          duration: 1,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "circ.out",
+        },
+        "-=0.5"
+      )
+      .from(
+        titleSplit.chars,
+        {
+          yPercent: 200,
+          stagger: 0.02,
+          ease: "power2.out",
+        },
+        "-=0.5"
+      );
+
+    const heroTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero-container",
+        start: "1% top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    heroTl.to(".hero-container", {
+      rotate: 7,
+      scale: 0.9,
+      yPercent: 30,
+      ease: "power1.inOut",
+    });
+  });
+  return (
+    <section className="bg-main-bg">
+      <div className="hero-container">
+        <img
+          src="/images/static-img.png"
+          alt="hero-img"
+          className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
+        />
+        <div className="hero-content opacity-0">
+          <div className="overflow-hidden">
+            <h1 className="hero-title">Freaking Delicious</h1>
+          </div>
+          <div
+            style={{
+              clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+            }}
+            className="hero-text-scroll"
+          >
+            <div className="hero-subtitle">
+              <h1>Protein + Caffine</h1>
+            </div>
+          </div>
+
+          <h2>
+            Live life to the fullest with SPYLT: Shatter boredom and embrace
+            your inner kid withh every deliciously smooth chug.
+          </h2>
+          <div className="hero-button">
+            <p>Chug a SPYLT</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
